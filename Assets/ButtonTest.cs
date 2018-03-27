6509299793c313d6421dc50032626bc7c4a2b634
Thiemo Krause <thiemok@users.noreply.github.com>
2018-03-27 10:13:42 +0200

    chore(Tests): add test scene

---
+++ Assets/ButtonTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MixedRealityToolkit.UX.Buttons;
+
+public class ButtonTest : MonoBehaviour {
+
+    uint frame = 0;
+
+	// Use this for initialization
+	void Start () {
+        Button button = this.GetComponent<Button>();
+        button.OnButtonClicked += (target) => {
+            Debug.LogFormat("Frame {0}\n{1}", frame, System.Environment.StackTrace);
+        };
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        frame++;
+	}
+}
